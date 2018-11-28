eabc373b4a2f64ecdbcf933453680a51bdbb5876
such <suchen9209@126.com>
2018-11-28 18:40:24 +0800

    发送什么就返回什么

--- testsocket.php
+++ testsocket.php
@@ -6,7 +6,7 @@ use Workerman\Worker;
 $ws_worker = new Worker("websocket://0.0.0.0:2346");
 
 // 4 processes
-$ws_worker->count = 4;
+$ws_worker->count = 1;
 
 // Emitted when new connection come
 $ws_worker->onConnect = function($connection)
@@ -15,10 +15,14 @@ $ws_worker->onConnect = function($connection)
  };
 
 // Emitted when data received
-$ws_worker->onMessage = function($connection, $data)
+$ws_worker->onMessage = function($connection, $data)use($ws_worker)
 {
     // Send hello $data
-    $connection->send('hello ' . $data);
+    //$connection->send('hello ' . $data);
+    foreach($ws_worker->connections as $connection)
+    {
+        $connection->send($data);
+    }
 };
 
 // Emitted when connection closed
