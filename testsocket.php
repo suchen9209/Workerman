99c2132fedee80106b9661728efe35c7894e9355
such <suchen9209@126.com>
2018-11-29 13:54:02 +0800

    增加ssl测试

--- testsocket.php
+++ testsocket.php
@@ -3,8 +3,20 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Workerman\Worker;
 
 global $ws_worker;
+
+$context = array(
+    // 更多ssl选项请参考手册 http://php.net/manual/zh/context.ssl.php
+    'ssl' => array(
+        // 请使用绝对路径
+        'local_cert'                 => '/usr/local/nginx/conf/cert/1540503872600.pem', // 也可以是crt文件
+        'local_pk'                   => '/usr/local/nginx/conf/cert/1540503872600.key',
+        'verify_peer'                => false,
+        // 'allow_self_signed' => true, //如果是自签名证书需要开启此选项
+    )
+);
 // Create a Websocket server
-$ws_worker = new Worker("websocket://0.0.0.0:2346");
+$ws_worker = new Worker("websocket://0.0.0.0:2346",$context);
+$ws_worker->transport = 'ssl';
 
 // 4 processes
 $ws_worker->count = 1;
